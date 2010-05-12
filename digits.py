d1df128ba7598360b3eb8e891f1608252a2085fd
Greg Hewgill <greg@hewgill.com>
2010-05-12 23:10:11 +1200

    slightly thicker digit segments

--- digits.py
+++ digits.py
@@ -7,13 +7,13 @@ Digits = [
 ]
 
 Segments = [
-    "M 3 1 L 17 1 14 4 6 4 Z",
-    "M 2 3 L 5 6 5 11 2 14 Z",
-    "M 18 3 L 18 14 15 11 15 6 Z",
-    "M 6 13 L 14 13 16 14.5 14 16 6 16 4 14.5 Z",
-    "M 2 15 L 5 18 5 23 2 26 Z",
-    "M 18 15 L 18 26 15 23 15 18 Z",
-    "M 3 28 L 17 28 14 25 6 25 Z",
+    "M 3 1 L 17 1 13 5 7 5 Z",                      # top
+    "M 2 3 L 6 7 6 10 2 14 Z",                      # left upper
+    "M 18 3 L 18 14 14 10 14 7 Z",                  # right upper
+    "M 6.5 12.5 L 13.5 12.5 16 14.5 13.5 16.5 6.5 16.5 4 14.5 Z",   # middle
+    "M 2 15 L 6 19 6 22 2 26 Z",                    # left lower
+    "M 18 15 L 18 26 14 22 14 19 Z",                # right lower
+    "M 3 28 L 17 28 13 24 7 24 Z",                  # bottom
 ]
 
 for i in range(len(Digits[0])):
@@ -36,7 +36,7 @@ for i in range(len(Digits[0])):
 
 f = open("decimal.svg", "w")
 print >>f, """<?xml version="1.0"?>
-    <svg width="3" height="3">
+    <svg width="4" height="4">
         <rect width="100%" height="100%" fill="black" />
     </svg>"""
 f.close()
