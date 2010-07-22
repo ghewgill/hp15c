26476fcc38eff3507bc41783c06a8cca7fca1ae2
Greg Hewgill <greg@hewgill.com>
2010-07-22 22:34:49 +1200

    build script for dashboard widget

---
+++ dashboard/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+DEST=HP15C.wdgt
+
+rm -rf $DEST
+mkdir $DEST
+cp ../15.jpg $DEST/
+cp ../*.png $DEST/
+cp ../sprintf-0.6.js ../hp15c.js $DEST/
+mkdir $DEST/jsmat
+cp ../jsmat/matrix.js $DEST/jsmat/
+cp Default.png $DEST/
+cp Info.plist $DEST/
+cp hp15c.html $DEST/
