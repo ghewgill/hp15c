ad761c6649a625e880ea84635fd3d8d7d6189566
Greg Hewgill <greg@hewgill.com>
2012-12-09 20:10:25 +1300

    add iphone 5 screenshots

--- ios/Free15c/Free15c/mkdefault.sh
+++ ios/Free15c/Free15c/mkdefault.sh
@@ -17,5 +17,7 @@ convert ../../../images/calc@2x.jpg \
     -page +542+40 ../../../images/ios/8@2x.png \
     -page +588+40 ../../../images/ios/9@2x.png \
     -layers flatten screenshot-front.jpg
+convert -size 1136x600 canvas:black -page +88+0 screenshot-front.jpg -layers flatten screenshot-front-568h.jpg
 
-convert -size 960x640 canvas:black -page +0+70 ../../../images/back@2x.jpg -geometry 960 -layers flatten screenshot-back.jpg
+convert -size 960x600 canvas:black -page +0+55 ../../../images/back@2x.jpg -geometry 960 -layers flatten screenshot-back.jpg
+convert -size 1136x600 canvas:black ../../../images/back@2x.jpg -geometry 1136 -layers flatten screenshot-back-568h.jpg
