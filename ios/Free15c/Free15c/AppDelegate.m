3d0132db0bf0a8f97755bf2b976e5387b129c237
Greg Hewgill <greg@hewgill.com>
2017-04-20 06:35:05 +1200

    Revert "add key click based on system setting, fixes #12"
    This reverts commit 9e88556f44ea5cdb284d699e7099e452c4ba4137.

--- ios/Free15c/Free15c/AppDelegate.m
+++ ios/Free15c/Free15c/AppDelegate.m
@@ -37,7 +37,6 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    CFPreferencesAppSynchronize(CFSTR("/var/mobile/Library/Preferences/com.apple.preferences.sounds"));
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
